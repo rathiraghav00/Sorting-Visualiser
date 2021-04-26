7dac810f181fe3098779af08fd77c66c05c6b542
rathiraghav00 <42902117+rathiraghav00@users.noreply.github.com>
2021-04-27 02:34:18 +0530

    Insertion sort added

--- src/SortingVisualizer/SortingVisualizer.jsx
+++ src/SortingVisualizer/SortingVisualizer.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { getMergeSortAnimations } from "../sortingAlgorithms/mergeSortAlgorithm.js";
 import { getBubbleSortAnimations } from "../sortingAlgorithms/bubbleSortAlgorithm.js";
+import { getInsertionSortAnimations } from "../sortingAlgorithms/insertionSortAlgorithm.js";
 import "./SortingVisualizer.css";
 
 // Change this value for the speed of the animations.
-const ANIMATION_SPEED_MS = 5;
+const ANIMATION_SPEED_MS = 100;
 
 // Change this value for the number of bars (value) in the array.
 const NUMBER_OF_ARRAY_BARS = 15;
@@ -34,6 +35,7 @@ export default class SortingVisualizer extends React.Component {
       array.push(randomIntFromInterval(5, 730));
     }
     this.setState({ array });
+    console.log("Size of array", NUMBER_OF_ARRAY_BARS);
   }
 
   mergeSort() {
@@ -101,7 +103,38 @@ export default class SortingVisualizer extends React.Component {
     // We leave it as an exercise to the viewer of this code to implement this method.
   }
 
-  insertionSort() {}
+  insertionSort() {
+    console.log("Inside Insertion Sort");
+    const animations = getInsertionSortAnimations(this.state.array);
+    var cnt = 0;
+    for (let i = 0; i < animations.length; i++) {
+      const arrayBars = document.getElementsByClassName("array-bar");
+      const isColorChange = Boolean(animations[i].length === 2);
+      if (isColorChange) {
+        cnt++;
+        console.log("2 wali condiition");
+        const [barOneIdx, barTwoIdx] = animations[i];
+        const barOneStyle = arrayBars[barOneIdx].style;
+        const barTwoStyle = arrayBars[barTwoIdx].style;
+        const color = cnt % 2 === 1 ? SECONDARY_COLOR : PRIMARY_COLOR;
+        setTimeout(() => {
+          barOneStyle.backgroundColor = color;
+          barTwoStyle.backgroundColor = color;
+        }, i * ANIMATION_SPEED_MS);
+      } else {
+        setTimeout(() => {
+          const [barOneIdx, newHeightOne, barTwoIdx, newHeightTwo] = animations[
+            i
+          ];
+          console.log("4 wali condiition");
+          const barOneStyle = arrayBars[barOneIdx].style;
+          barOneStyle.height = `${newHeightOne}px`;
+          const barTwoStyle = arrayBars[barTwoIdx].style;
+          barTwoStyle.height = `${newHeightTwo}px`;
+        }, i * ANIMATION_SPEED_MS);
+      }
+    }
+  }
 
   selectionSort() {
     this.mergeSort();
